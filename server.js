8678d853c33dfc698f0891da4f32fa9ab4fb3dc0
Kazakov Ruslan <ruslan.kazakov@gmail.com>
2014-12-02 18:54:41 +0600

    adding sample bible passage parser code

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var wines = require('./model');
 
 var app = express();
 
-app.get('/api/employees/:id/reports', wines.findByManager);
-app.get('/api/employees/:id', wines.findById);
-app.get('/api/employees', wines.findAll);
+app.get('/:passage', wines.findAll);
+
+//app.get('/api/employees/:id/reports', wines.findByManager);
+//app.get('/api/employees/:id', wines.findById);
+//app.get('/employees', wines.findAll);
 
 app.listen(3000);
 
