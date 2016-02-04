7941bf9f36d29b9d9f85bd510b6c39812d2ae85b
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-02-04 11:18:02 +1100

    Adding gulp live javascript reload

--- restify.js
+++ restify.js
@@ -6,13 +6,13 @@ function respond(req, res, next) {
 }
 
 function respondJson(req, res, next) {
-  res.send({ name: req.params.name });
+  res.send({ name: req.params.name, greeting: 'hello' });
   next();
 }
 
 var server = restify.createServer();
 server.get('/hello/:name', respondJson);
-//server.head('/hello/:name', respond);
+server.head('/hello/:name', respond);
 
 server.listen(8000, function() {
   console.log('%s listening at %s', server.name, server.url);
