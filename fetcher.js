3d45809d86dc14d39a9fd8d241f7a7f1ac5f8762
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-02-11 14:09:41 +1100

    Modularize fetch logic

---
+++ fetcher.js
@@ -0,0 +1,149 @@
+exports.fetchBcv = function fetchBcv(passage, type, callback) {
+  var mongoQuery = [];
+  var translations = [];
+  if (passage.translations != null) {
+    for (var i=0; i<passage.translations.length; i++) {
+      translations.push(passage.translations[i].osis);
+    }
+  } // no translations set
+  else {
+    translations.push('RUSV'); // default
+  }
+  if (type === 'b') {
+    mongoQuery.push({
+      'tran':translations[0],
+      'bookRef':passage.start.b
+    });
+  }
+  else if (type === 'bc') {
+    mongoQuery.push({
+      'tran':translations[0],
+      'bookRef':passage.start.b,
+      'chapter':passage.start.c
+    });
+  }
+  else if (type === 'bcv') {
+    for (var i=0; i<translations.length; i++) {
+      mongoQuery.push({
+        'tran':translations[i],
+        'bookRef':passage.start.b,
+        'chapter':passage.start.c,
+        'verse':passage.start.v
+      });
+    }
+  }
+  return callback(null, mongoQuery);
+};
+
+exports.fetchRange = function fetchRange(bcv, passage, callback) {
+  var mongoQuery = [];
+  var translationInfo = bcv.translation_info("");
+
+  var pTranslation = 'RUSV';
+  if (passage.translations != null) {
+    pTranslation = passage.translations[0].osis;
+  }
+
+  // passage of the same book
+  if (passage.start.b === passage.end.b) {
+    // passage of one chapter
+    if (passage.start.c === passage.end.c) {
+      mongoQuery.push({
+        'tran':pTranslation,
+        'bookRef':passage.start.b,
+        'chapter':passage.start.c,
+        'verse':{$gte:passage.start.v, $lte:passage.end.v}
+      });
+    }
+    else { // passage of many chapters
+      var chapters = [];
+      for (var ch=passage.start.c; ch<=passage.end.c; ch++) {
+        var startVerse = 1;
+        var endVerse = translationInfo.chapters[passage.start.b][ch-1];
+
+        if (ch === passage.start.c) {
+          startVerse = passage.start.v;
+        }
+        if (ch === passage.end.c) {
+          endVerse = passage.end.v;
+        }
+
+        chapters.push({
+          'tran':pTranslation,
+          'bookRef':passage.start.b,
+          'chapter':ch,
+          'verse':{$gte:startVerse, $lte:endVerse}
+        });
+      }
+
+      mongoQuery.push({
+        $or: chapters
+      });
+    }
+  } // passage of many books
+  else {
+    var chapters = [];
+    var startBook = translationInfo.order[passage.start.b];
+    var endBook = translationInfo.order[passage.end.b];
+
+    for (var b=startBook; b<=endBook; b++) {
+      var bookRef = translationInfo.books[b-1];
+
+      // passage includes the whole book in the middle
+      if (b != startBook && b != endBook) {
+        chapters.push({
+          'tran':pTranslation,
+          'bookRef':bookRef
+        });
+      }
+      else { // other half-books parts
+        var startChapter = 1;
+        var endChapter = translationInfo.chapters[bookRef].length;
+
+        if (b === startBook) {
+          startChapter = passage.start.c;
+        }
+        if (b === endBook) {
+          endChapter = passage.end.c;
+        }
+
+        for (var ch=startChapter; ch<=endChapter; ch++) {
+          var startVerse = 1;
+          var endVerse = translationInfo.chapters[bookRef][ch-1];
+
+          // first chapter, first verse in range
+          if (b === startBook && ch === startChapter) {
+            startVerse = passage.start.v;
+            chapters.push({
+              'tran':pTranslation,
+              'bookRef':bookRef,
+              'chapter':startChapter,
+              'verse':{$gte:startVerse, $lte:endVerse}
+            });
+          } // last chapter, last verse in range
+          else if (b === endBook && ch === endChapter) {
+            endVerse = passage.end.v;
+            chapters.push({
+              'tran':pTranslation,
+              'bookRef':bookRef,
+              'chapter':ch,
+              'verse':{$gte:startVerse, $lte:endVerse}
+            });
+          }
+          else { // other whole chapters in range
+            chapters.push({
+              'tran':pTranslation,
+              'bookRef':bookRef,
+              'chapter':ch
+            });
+          }
+        }
+      }
+    }
+
+    mongoQuery.push({
+      $or: chapters
+    });
+  }
+  return callback(null, mongoQuery);
+};
--- model.js
+++ model.js
@@ -1,159 +1,10 @@
 var mongo = require('./mongo');
+var fetcher = require('./fetcher');
 
-//var bcv_parser = require("./full_bcv_parser.js").bcv_parser;
 var bcv_parser = require("bible-passage-reference-parser/js/ru_bcv_parser").bcv_parser;
 var bcv = new bcv_parser;
 
 var mongoQuery = [];
-
-function fetchBcv(passage, type) {
-  var translations = [];
-  if (passage.translations != null) {
-    for (var i=0; i<passage.translations.length; i++) {
-      translations.push(passage.translations[i].osis);
-    }
-  } // no translations set
-  else {
-    translations.push('RUSV'); // default
-  }
-
-  //console.log(translations);
-
-  if (type === 'b') {
-    mongoQuery.push({
-      'tran':translations[0],
-      'bookRef':passage.start.b
-    });
-  }
-  else if (type === 'bc') {
-    mongoQuery.push({
-      'tran':translations[0],
-      'bookRef':passage.start.b,
-      'chapter':passage.start.c
-    });
-  }
-  else if (type === 'bcv') {
-    for (var i=0; i<translations.length; i++) {
-      mongoQuery.push({
-        'tran':translations[i],
-        'bookRef':passage.start.b,
-        'chapter':passage.start.c,
-        'verse':passage.start.v
-      });
-    }
-  }
-};
-
-function fetchRange(passage) {
-  var translationInfo = bcv.translation_info("");
-
-  var pTranslation = 'RUSV';
-  if (passage.translations != null) {
-    pTranslation = passage.translations[0].osis;
-  }
-
-  // passage of the same book
-  if (passage.start.b === passage.end.b) {
-    // passage of one chapter
-    if (passage.start.c === passage.end.c) {
-      mongoQuery.push({
-        'tran':pTranslation,
-        'bookRef':passage.start.b,
-        'chapter':passage.start.c,
-        'verse':{$gte:passage.start.v, $lte:passage.end.v}
-      });
-    }
-    else { // passage of many chapters
-      var chapters = [];
-      for (var ch=passage.start.c; ch<=passage.end.c; ch++) {
-        var startVerse = 1;
-        var endVerse = translationInfo.chapters[passage.start.b][ch-1];
-
-        if (ch === passage.start.c) {
-          startVerse = passage.start.v;
-        }
-        if (ch === passage.end.c) {
-          endVerse = passage.end.v;
-        }
-
-        chapters.push({
-          'tran':pTranslation,
-          'bookRef':passage.start.b,
-          'chapter':ch,
-          'verse':{$gte:startVerse, $lte:endVerse}
-        });
-      }
-
-      mongoQuery.push({
-        $or: chapters
-      });
-    }
-  } // passage of many books
-  else {
-    var chapters = [];
-    var startBook = translationInfo.order[passage.start.b];
-    var endBook = translationInfo.order[passage.end.b];
-
-    for (var b=startBook; b<=endBook; b++) {
-      var bookRef = translationInfo.books[b-1];
-
-      // passage includes the whole book in the middle
-      if (b != startBook && b != endBook) {
-        chapters.push({
-          'tran':pTranslation,
-          'bookRef':bookRef
-        });
-      }
-      else { // other half-books parts
-        var startChapter = 1;
-        var endChapter = translationInfo.chapters[bookRef].length;
-
-        if (b === startBook) {
-          startChapter = passage.start.c;
-        }
-        if (b === endBook) {
-          endChapter = passage.end.c;
-        }
-
-        for (var ch=startChapter; ch<=endChapter; ch++) {
-          var startVerse = 1;
-          var endVerse = translationInfo.chapters[bookRef][ch-1];
-
-          // first chapter, first verse in range
-          if (b === startBook && ch === startChapter) {
-            startVerse = passage.start.v;
-            chapters.push({
-              'tran':pTranslation,
-              'bookRef':bookRef,
-              'chapter':startChapter,
-              'verse':{$gte:startVerse, $lte:endVerse}
-            });
-          } // last chapter, last verse in range
-          else if (b === endBook && ch === endChapter) {
-            endVerse = passage.end.v;
-            chapters.push({
-              'tran':pTranslation,
-              'bookRef':bookRef,
-              'chapter':ch,
-              'verse':{$gte:startVerse, $lte:endVerse}
-            });
-          }
-          else { // other whole chapters in range
-            chapters.push({
-              'tran':pTranslation,
-              'bookRef':bookRef,
-              'chapter':ch
-            });
-          }
-        }
-      }
-    }
-
-    mongoQuery.push({
-      $or: chapters
-    });
-  }
-};
 
 function reformatResults(items) {
   var text = [];
@@ -180,28 +31,36 @@ exports.parsePassage = function(req, res) {
     // whole chapter or verse
     else if (entity.type === 'bc' || entity.type === 'bcv') {
       // bcv has only one passage
-      fetchBcv(entity.passages[0], entity.type);
+      fetcher.fetchBcv(entity.passages[0], entity.type, function(err, result) {
+        mongoQuery = result;
+      });
     } // range of verses
     else if (entity.type === 'range') {
       // range has only one passage
-      fetchRange(entity.passages[0]);
+      fetcher.fetchRange(bcv, entity.passages[0], function(err, result) {
+        mongoQuery = result;
+      });
     } // sequence of passages
     else if(entity.type === 'sequence') {
       for (var j=0; j<entity.passages.length; j++) {
         var passage = entity.passages[j];
         // passage sequence includes bcv
         if (passage.type === 'bcv' || passage.type === 'integer') {
-          fetchBcv(passage, 'bcv');
+          fetcher.fetchBcv(passage, 'bcv', function(err, result) {
+            mongoQuery = result;
+          });
         } // passage sequence includes range
         else if(passage.type === 'range') {
-          fetchRange(passage);
+          fetcher.fetchRange(bcv, passage, function(err, result) {
+            mongoQuery = result;
+          });
         }
       }
     }
   }
 
   if (mongoQuery.length > 0) {
-    mongo.connect(function(err, collection) {
+    mongo.open(function(err, collection) {
       collection.find({ $or: mongoQuery }, { _id: 0 })
         .sort({chapter: 1, verse: 1})
         .toArray(function(err, items) {
@@ -211,9 +70,6 @@ exports.parsePassage = function(req, res) {
     });
   }
   else {
-    var error = {
-      message: 'Bible passage is not found.'
-    };
-    res.send(error);
+    res.send({ error: 'Bible passage is not found.' });
   }
 };
