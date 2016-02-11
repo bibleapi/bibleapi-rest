87e9c530ef90276165e1908de4425db857cb9abd
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-02-11 14:08:03 +1100

    Cleaning entry point file

--- restify.js
+++ restify.js
@@ -2,21 +2,11 @@ var restify = require('restify');
 var parsePassage = require('./model').parsePassage;
 var meta = require('./meta');
 
-// function respond(req, res, next) {
-//   res.send('hello ' + req.params.name);
-//   next();
-// }
-//
-// function respondJson(req, res, next) {
-//   res.send({ name: req.params.name, greeting: 'hello' });
-//   next();
-// }
-
 function respondIndex(req, res, next) {
   res.set('Content-Type', 'text/html');
   res.end('<html>\n' +
   '  <head><title>BibleAPI</title></head>\n' +
-  '  <body>Bible API web service v0.0.6</body>\n</html>');
+  '  <body>Bible API web service v0.0.7</body>\n</html>');
   next();
 };
 
@@ -31,11 +21,10 @@ function getMetaData(req, res, next) {
 };
 
 var server = restify.createServer();
-// server.get('/hello/:name', respondJson);
-// server.head('/hello/:name', respond);
 
+// index
 server.get('/', respondIndex);
-//server.head('/', respondIndex);
+server.head('/', respondIndex);
 
 // metadata
 server.get('/api/v1.0/meta/:translation', getMetaData);
