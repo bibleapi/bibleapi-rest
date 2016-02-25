9efda12526bd22ece1f2ff755b2d3975770be61e
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-02-26 09:53:33 +1100

    Version bump up

--- server/router.js
+++ server/router.js
@@ -5,7 +5,7 @@ exports.respondIndex = function respondIndex(req, res, next) {
   res.set('Content-Type', 'text/html');
   res.end('<html>\n' +
     '<head><title>BibleAPI</title></head>\n' +
-    '<body>Bible API web service v0.0.9</body>\n</html>');
+    '<body>Bible API web service v0.1.0</body>\n</html>');
   next();
 };
 
